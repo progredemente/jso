b0474d87f74c54b8e5b14c0222b6d03a7825de78
Oktopoid <orson_wells40@hotmail.com>
2023-12-30 14:23:53 +0100

    Implemented simple victory  system

--- src/components/main-board/main-board.vue
+++ src/components/main-board/main-board.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="main-board">
-    <img
-      class="art-piece"
-      src="https://omnesmag.com/wp-content/uploads/2016/05/vangogh.jpg"
-      alt=""
-    />
+    <h1 v-if="!complete">{{ progress }}%</h1>
+    <h1 v-else>¡Complete!</h1>
+    <h2>{{ squaresTouched }}: {{ squareCount }}</h2>
+    <img class="art-piece" :src="src" alt="" />
     <div class="main-board-content">
       <main-board-square
         v-for="(square, index) in squareCount"
@@ -12,6 +11,8 @@
         :index="index"
         :color="squareColor"
         :size="squareSize"
+        :block="complete"
+        @touched="squaresTouched++"
       />
     </div>
   </div>
@@ -20,7 +21,7 @@
 <script setup>
 import MainBoardSquare from "./main-board-square.vue";
 import { useRandomColor } from "../../composables";
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 // import { configStore } from "../../stores/configStore.js";
 
 // const config = configStore();
@@ -28,22 +29,38 @@ import { ref, computed } from "vue";
 const props = defineProps({
   cols: { type: [Number, String], default: 30 },
   rows: { type: [Number, String], default: 18 },
+  src: { type: String },
+  victory: { type: Number, default: 15 },
 });
 
-const squareSize = computed(() => 30);
+const emit = defineEmits(["board-completed"]);
+
 const squareColor = useRandomColor();
+const squaresTouched = ref(0);
 const completed = ref(false);
 
+const squareSize = computed(() => 30);
 const cols = computed(() => Number(props.cols));
 const rows = computed(() => Number(props.rows));
 const boardWidth = computed(() => `${squareSize.value * cols.value}px`);
 const boardHeight = computed(() => `${squareSize.value * rows.value}px`);
 const squareCount = computed(() => cols.value * rows.value);
+const progress = computed(() => {
+  const prog = (squaresTouched.value / squareCount.value) * 100;
+  return prog.toFixed(1);
+});
+const complete = computed(() => progress.value >= props.victory);
 
 console.log("BOARD", squareSize.value);
 
 const gridTemplateColumns = computed(() => `repeat(${cols.value}, 1fr)`);
 const gridTemplateRows = computed(() => `repeat(${rows.value}, 1fr)`);
+
+watch(squaresTouched, () => {
+  if (complete.value) {
+    emit("complete");
+  }
+});
 </script>
 
 <style scoped>
